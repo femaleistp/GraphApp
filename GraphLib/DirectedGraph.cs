72a1d4a5e9231f4ca38a83e5afc03241702c8659
Brittany H <femaleentp@gmail.com>
2024-05-29 11:52:01 -0700

    started Dijkstra algorithm method with MinDistance method in DirectedGraph class.

--- GraphLib/DirectedGraph.cs
+++ GraphLib/DirectedGraph.cs
@@ -1,4 +1,5 @@
-﻿namespace GraphLib
+﻿
+namespace GraphLib
 {
     public class DirectedGraph
     {
@@ -69,5 +70,69 @@
                 Console.WriteLine();
             }
         }
+
+        public void Dijkstra(int[,] graph, int src)
+        {
+            // set up some buckets to store our info
+            int[] dist = new int[Vertices.Count];
+            bool[] visits = new bool[Vertices.Count];
+
+            // initialize the arrays
+            for (int i= 0; i < Vertices.Count; i++)
+            {
+                dist[i] = int.MaxValue;
+                visits[i] = false;
+            }
+
+            dist[src] = 0;
+
+            for(int count =0; count < Vertices.Count -1; count++)
+            {
+                // pick minimum distance vertex from the set of vertices not yet processed
+                int u = MinDistance(dist, visits);
+
+                visits[u] = true;
+
+                // update dist value of the adjacent vertices of the picked vertex
+                for (int v = 0; v < Vertices.Count; v++)
+                {
+                    /*
+                    update dist[v] only if the following conditions are true;
+                       it is not visited
+                       there exists an edge from u to v
+                       the edge is not infinity,
+                       the total weight of the path from src to v through the picked node (u) is smaller than the current value of dist[v]
+                     */
+
+                    if (!visits[v] && 
+                        graph[u,v] != 0 &&
+                        dist[u] != int.MaxValue &&
+                        dist[u] + graph[u,v] < dist[v]
+                        )
+                    {
+                        dist[v] = dist[u] + graph[u, v]
+                    }
+                }
+            }
+        }
+
+        private int MinDistance(int[] dist, bool[] visits)
+        {
+            int min = int.MaxValue;
+            int min_index = -1;
+
+            for (int i = 0; i < Vertices.Count; i++)
+            {
+                if (visits[i] == false && dist[i] <= min)
+                {
+                    min = dist[i];
+                    min_index = i;
+
+                }
+            }
+
+            return min_index;
+        }
     }
+
 }
